49bc846380b32e42790bc7e5158526faca75dfe0
Eduardolg1 <93345432+Eduardolg1@users.noreply.github.com>
2024-03-08 09:29:02 -0300

    Atualizar o abs.php

--- Desinvolvimento de sistemas/eduardo/abs.php
+++ Desinvolvimento de sistemas/eduardo/abs.php
@@ -1,5 +1,22 @@
-<?php
-$numero = -5;
-$abesoluto(abs($numero));
-echo "o valor $numero fica $absuluto quando uilisamos o abs";
-?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Caixa de Inserir Valor e Converter Base</title>
+</head>
+<body>
+
+<form method="post" action="processar.php">
+    <label for="valor">Insira o valor:</label>
+    <input type="number" id="valor" name="valor">
+    <label for="base_origem">Base de origem:</label>
+    <select id="base_origem" name="base_origem">
+        <option value="2">Binário</option>
+        <option value="8">Octal</option>
+        <option value="10" selected>Decimal</option>
+        <option value="16">Hexadecimal</option>
+    </select>
+    <input type="submit" value="Converter">
+</form>
+
+</body>
+</html>
